1ed429fc17c12d9b57c700f0452201075dc87b58
Your Name~manojprabhakar24 <manojptn16@gmail.com>
2023-10-19 09:20:31 +0530

    working

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:camera_demo/Mycamera.dart';
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final cameras = await availableCameras();
+
+  final firstCamera = cameras.first;
+
+  runApp(MaterialApp(
+    theme: ThemeData.light(),
+    home: TakePictureScreen(
+      camera: firstCamera,
+    ),
+  ));
+}
